3cf1d0dcf8d7469a533bab18e1e51af92fe5cbf8
cdickson <cdickson@uncharted.software>
2024-04-24 09:48:31 -0400

    add job to run tests

---
+++ server/src/test/java/ServerApplicationTests.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = ServerApplicationTests.class)
+public class ServerApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+}
